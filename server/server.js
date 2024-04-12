587be76762518066801cba6c58b44c71262545f1
Ryan Graham <rgraham@harriscomputer.com>
2024-04-12 09:47:09 -0400

    create simple server

---
+++ server/server.js
@@ -0,0 +1,18 @@
+// Import express
+const express = require('express');
+
+// Create an express application
+const app = express();
+
+// Define a port
+const PORT = process.env.PORT || 3000;
+
+// Define a route for GET requests to '/'
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Start the server on the specified port
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
